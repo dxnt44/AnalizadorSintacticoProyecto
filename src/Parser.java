929873559f7dce07c1862e8a687855505db0a52d
dxnt44 <trejod40@gmail.com>
2023-06-01 04:47:58 -0600

    Tokens y funciones de prueba
    Creacion de tokens y creacion de primeras 2 funciones

--- src/Parser.java
+++ src/Parser.java
@@ -5,12 +5,36 @@ public class Parser {
     private final List<Token> tokens;
 
     private final Token identificador = new Token(TipoToken.IDENTIFICADOR, "");
-    private final Token select = new Token(TipoToken.SELECT, "select");
-    private final Token from = new Token(TipoToken.FROM, "from");
-    private final Token distinct = new Token(TipoToken.DISTINCT, "distinct");
+    private final Token and = new Token(TipoToken.Y, "Y");
+    private final Token class_ = new Token(TipoToken.CLASE, "CLASE");
+    private final Token else_ = new Token(TipoToken.ADEMAS, "ADEMAS");
+    private final Token false_ = new Token(TipoToken.FALSO, "FALSO");
+    private final Token for_ = new Token(TipoToken.PARA, "PARA");
+    private final Token fun = new Token(TipoToken.FUN, "FUN");
+    private final Token null_ = new Token(TipoToken.SI, "NULO");
+    private final Token or_ = new Token(TipoToken.O, "O");
+    private final Token print_ = new Token(TipoToken.IMPRIMIR, "IMPRIMIR");
+    private final Token return_ = new Token(TipoToken.RETORNAR, "RETORNAR");
+    private final Token super_ = new Token(TipoToken.SUPER, "SUPER");
+    private final Token this_ = new Token(TipoToken.ESTE, "ESTE");
+    private final Token true_ = new Token(TipoToken.VERDADERO, "VERDADERO");
+    private final Token var = new Token(TipoToken.VAR, "VAR");
+    private final Token while_ = new Token(TipoToken.MIENTRAS, "MIENTRAS");
+    private final Token parentesis_izq = new Token(TipoToken.PARENTESIS_IZQUIERDO, "(");
+    private final Token parentesis_der = new Token(TipoToken.PARENTESIS_DERECHO, ")");
+    private final Token llave_izq = new Token(TipoToken.LLAVE_IZQUIERDA, "{");
+    private final Token llave_der = new Token(TipoToken.LLAVE_DERECHA, "}");
     private final Token coma = new Token(TipoToken.COMA, ",");
     private final Token punto = new Token(TipoToken.PUNTO, ".");
-    private final Token asterisco = new Token(TipoToken.ASTERISCO, "*");
+    private final Token punto_y_coma = new Token(TipoToken.PUNTO_Y_COMA, ";");
+    private final Token suma = new Token(TipoToken.SUMA, "+");
+    private final Token resta = new Token(TipoToken.RESTA, "-");
+    private final Token multiplicacion = new Token(TipoToken.MULTIPLICACION, "*");
+    private final Token division = new Token(TipoToken.DIVISION, "/");
+    private final Token negacion = new Token(TipoToken.NEGACION, "!");
+    private final Token asignacion = new Token(TipoToken.ASIGNACION, "=");
+    private final Token menor_que = new Token(TipoToken.MENOR_QUE, "<");
+    private final Token mayor_que = new Token(TipoToken.MAYOR_QUE, ">");
     private final Token finCadena = new Token(TipoToken.EOF, "");
 
     private int i = 0;
@@ -41,6 +65,24 @@ public class Parser {
         }*/
     }
 
+    void PROGRAM(){
+        if(hayErrores) return;
+
+       if(preanalisis.equals(identificador)){
+           DECLARATION();
+       }else{
+           hayErrores = true;
+           System.out.println("Error en la posición " + preanalisis.posicion + ". Se esperaba un identificador.");
+       }
+    }
+
+    void DECLARATION(){
+        if (hayErrores) return;
+
+        if (preanalisis.equals(identificador)){
+            CLASS_DECL()
+        }
+    }
     void Q(){
         if(preanalisis.equals(select)){
             coincidir(select);
