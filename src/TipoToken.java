59038e55285d09c7c9a1bc42426f352c6d29fbcd
dxnt44 <trejod40@gmail.com>
2023-06-01 02:48:11 -0600

    Cambios en TIpoToken
    Agregar Caracteres, ID y palabras reservadas a la clase

--- src/TipoToken.java
+++ src/TipoToken.java
@@ -1,12 +1,59 @@
 public enum TipoToken {
+    // identificador, cadena y número
     IDENTIFICADOR,
+    CADENA,
+    NUMERO,
+    REAL,
+    ENTERO,
 
     // Palabras reservadas
-    SELECT, FROM, DISTINCT,
+    Y,
+    CLASE,
+    ADEMAS,
+    FALSO,
+    PARA,
+    FUN,
+    SI,
+    NULO,
+    O,
+    IMPRIMIR,
+    RETORNAR,
+    SUPER,
+    ESTE,
+    VERDADERO,
+    VAR,
+    MIENTRAS,
 
     // Caracteres
-    COMA, PUNTO, ASTERISCO,
-
+    PARENTESIS_IZQUIERDO,
+    PARENTESIS_DERECHO,
+    LLAVE_IZQUIERDA,
+    LLAVE_DERECHA,
+    COMA,
+    PUNTO,
+    PUNTO_Y_COMA,
+    SUMA,
+    RESTA,
+    MULTIPLICACION,
+    DIVISION,
+    NEGACION,
+    DISTINTO,
+    ASIGNACION,
+    IGUALDAD,
+    MENOR_QUE,
+    MENOR_O_IGUAL_QUE,
+    MAYOR_QUE,
+    MAYOR_O_IGUAL_QUE,
+    COMENTARIO,
     // Final de cadena
     EOF
+
+
+
+
+
+
+
+
+
 }
